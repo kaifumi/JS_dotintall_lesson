7c1a13d2e5719545d8b6be5dee26fea0cdffef27
kaifumi <kaifumi1001@gmail.com>
2020-07-03 22:09:38 +0900

    [Add]#06 数値の計算をしてみよう

--- 01:basic_grammer/js/main.js
+++ 01:basic_grammer/js/main.js
@@ -1,5 +1,11 @@
 'use strict';
 
-console.log('Hello world from main.js!');
-console.log("it\'s me!")
-console.log('he\nllo w\torld')
+console.log(10 + 3); // 13
+console.log(10 - 3); // 7
+console.log(10 * 3); // 30
+console.log(10 / 3); // 3.3333
+console.log(10 % 3); // 1
+console.log(10 ** 3); // 1000
+
+console.log(2 + 10 * 3); // 32
+console.log((2 + 10) * 3); // 36
