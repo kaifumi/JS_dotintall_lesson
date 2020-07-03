e6908997ae42fb13a6a858d32b81ff37f655bad7
kaifumi <kaifumi1001@gmail.com>
2020-07-03 22:31:10 +0900

    [Add]#08 変数を使ってみよう

--- 01:basic_grammer/js/main.js
+++ 01:basic_grammer/js/main.js
@@ -1,11 +1,14 @@
 'use strict';
 
-// 定数
+// 定数 const
+// 変数 let
 
-// console.log(150 * 140);
-// console.log(150 * 160);
+let price = 150;
 
-const price = 150;
+console.log(price * 140);
+console.log(price * 160);
+
+price = 170;
 
 console.log(price * 140);
 console.log(price * 160);
