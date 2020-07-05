803e42a04da1d742b91ef9ce4a2977c965495494
kaifumi <kaifumi1001@gmail.com>
2020-07-05 17:26:59 +0900

    [Add]#19 continue､breakを使ってみよう

--- 01:basic_grammer/js/main.js
+++ 01:basic_grammer/js/main.js
@@ -1,8 +1,8 @@
 'use strict';
 
-let hp = -50;
-
-do {
-  console.log(`残りHPは${hp}です`);
-  hp -= 15;
-} while (hp > 0)
+for (let i = 1; i <= 10; i++) {
+  if (i === 4) {
+    break;
+  }
+  console.log(i);
+}
